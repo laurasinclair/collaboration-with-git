5ddd98aa321375b383c2683a61442a8f219c7f3b
Laura Sinclair <l.sinclair@tuta.io>
2025-08-07 15:50:59 +0200

    Refactored App() return function

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,13 @@ function App() {
 	const [theme, setTheme] = useState("theme_light");
 	const [isLoading, setIsLoading] = useState(false);
 
-	if (isLoading) {
-		return <Spinner />;
-	} 
-	else {
-		return (
-				<div className={"App " + theme}>
-					Hello there
-				</div>
-		);
-	}
+	return (
+		isLoading ? <Spinner /> : (
+			<div className={"App " + theme}>
+				Hello there
+			</div>
+		)
+	);
 }
 
 export default App;
